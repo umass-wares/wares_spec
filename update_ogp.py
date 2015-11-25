a6a3991b026ee13c51e54e15a147058ca4757052
Aleks <aleks@labmole.(none)>
2015-11-25 10:17:51 -0500

    update_ogp.py

---
+++ update_ogp.py
@@ -0,0 +1,20 @@
+from adc5g_cal import ADC5g_Calibration_Tools
+import numpy as np
+from corr import katcp_wrapper
+
+roach = katcp_wrapper.FpgaClient('172.30.51.97')
+adc_cal = ADC5g_Calibration_Tools(roach, clk=1600)
+
+df = np.load('ogp_default.npz')
+
+zdok0_ogp = df['zdok0_ogp']
+
+zdok = 0
+cores = [1,2,3,4]
+
+print
+print "Setting ogp for zdok0..."
+print zdok0_ogp
+print
+
+adc_cal.set_ogp(zdok, cores, zdok0_ogp)
