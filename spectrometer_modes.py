d25c5ba6c0ac1f2cefa32e27981eb0f42e159489
alpops <alpops@github.com>
2017-05-30 10:28:59 -0400

    latest bitcodes supported

--- spectrometer_modes.py
+++ spectrometer_modes.py
@@ -2,7 +2,7 @@ class mode_800(object):
 
     def __init__(self):
 
-        self.bitcode = 'adc5g_800mhz_qbs_4ch_2017_May_03_1259.bof.gz'
+        self.bitcode = 'adc5g_800mhz_qbs_4inbb_2017_May_26_1449.bof.gz'
         self.clk = 1600
         self.bandwidth = 800
         self.ADCstreams = 8
@@ -20,7 +20,7 @@ class mode_400(object):
 
     def __init__(self):
 
-        self.bitcode = 'adc5g_400mhz_qbs_1ch_2017_May_15_1407.bof.gz'
+        self.bitcode = 'adc5g_400mhz_qbs_4in_2017_May_16_1346.bof.gz'
         self.clk = 800
         self.bandwidth = 400
         self.ADCstreams = 8
@@ -33,3 +33,21 @@ class mode_400(object):
         self.FFTsize = self.numchannels*2
         self.FFTinputs = 8
         self.nbram = 4
+
+class mode_200(object):
+
+    def __init__(self):
+
+        self.bitcode = 'adc5g_200mhz_qbs_4in_2017_May_22_1352.bof.gz'
+        self.clk = 800
+        self.bandwidth = 200
+        self.ADCstreams = 4
+        self.numchannels = 8192
+        self.gain = 0xffffff
+        self.shift = 0xffff
+
+        self.sync_LCM = 12
+        self.pfb_taps = 4
+        self.FFTsize = self.numchannels*2
+        self.FFTinputs = 4
+        self.nbram = 2
